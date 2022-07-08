0fa9f52d8c57d0f71c748d0a50581929b7b2a57e
Colleen Murray <colleenkhm@gmail.com>
2022-07-08 08:29:43 -0500

    server wireframe

---
+++ server/seeders/seeds.js
@@ -0,0 +1,75 @@
+const faker = require('faker');
+
+const db = require('../config/connection');
+const { Entry, User } = require('../../module-21-server-starter/models');
+
+db.once('open', async () => {
+  await Entry.deleteMany({});
+  await User.deleteMany({});
+
+  // create user data
+  const userData = [];
+
+  for (let i = 0; i < 50; i += 1) {
+    const username = faker.internet.userName();
+    const email = faker.internet.email(username);
+    const password = faker.internet.password();
+
+    userData.push({ username, email, password });
+  }
+
+  const createdUsers = await User.collection.insertMany(userData);
+
+  // create supporters
+  for (let i = 0; i < 100; i += 1) {
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { _id: userId } = createdUsers.ops[randomUserIndex];
+
+    let friendId = userId;
+
+    while (friendId === userId) {
+      const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+      friendId = createdUsers.ops[randomUserIndex];
+    }
+
+    await User.updateOne({ _id: userId }, { $addToSet: { friends: friendId } });
+  }
+
+  // create entries
+  let createdEntries = [];
+  for (let i = 0; i < 100; i += 1) {
+    const entryText = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { username, _id: userId } = createdUsers.ops[randomUserIndex];
+
+    const createdEntry = await Entry.create({ entryText, username });
+
+    const updatedUser = await User.updateOne(
+      { _id: userId },
+      { $push: { entry: createdEntry._id } }
+    );
+
+    createdEntries.push(createdEntry);
+  }
+
+  // create reactions
+  // for (let i = 0; i < 100; i += 1) {
+  //   const reactionBody = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+
+  //   const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+  //   const { username } = createdUsers.ops[randomUserIndex];
+
+  //   const randomEntryIndex = Math.floor(Math.random() * createdEntries.length);
+  //   const { _id: thoughtId } = createdEntries[randomEntryIndex];
+
+  //   await Entry.updateOne(
+  //     { _id: entryId },
+  //     { $push: { reactions: { reactionBody, username } } },
+  //     { runValidators: true }
+  //   );
+  // }
+
+  console.log('all done!');
+  process.exit(0);
+});
