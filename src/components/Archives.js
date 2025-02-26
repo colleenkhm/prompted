a009b6aeeb6c9bdcffd421303f5f3fd78d43e700
Colleen Murray <colleenmurray@Colleens-MacBook-Pro.local>
2025-02-25 18:01:31 -0600

    add calendar to archive page

--- src/components/Archives.js
+++ src/components/Archives.js
@@ -1,8 +1,17 @@
+import { Grid2 } from "@mui/material";
 import React from "react";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
+import '../styles/Archives.css'
 
 function Archives () {
 return (
+    <>
     <h2>word calendar</h2>
+    <Grid2 container className='calendarContainer'>
+    <Calendar />
+    </Grid2>
+    </>
 )
 }
 
